c85fb4efb7013770a841ef93ae5c86496d386096
nakultanwar <nphayabusa334@gmail.com>
2026-03-25 00:20:42 +0530

    CH-02 Finish

--- CH-01/EP-01/script.js
+++ CH-01/EP-01/script.js
@@ -1,1 +1,71 @@
 console.log("Namste DSA");
+// Data Types
+/*
+1 Strings
+2 Numbers
+3 Boolean
+4 BigInt
+5 Null
+6 Symbol
+7 Undefined
+*/
+
+console.log("Hello World");
+console.log(10);
+console.log(true);
+
+
+// For Sum of 2 Nos.
+const a = 10;
+const b = 20;
+const sum = a + b;
+const mul = a * b;
+const sub = a - b;
+const div = a / b;
+console.log(`ADIITION is ${sum},
+SUBTRACTION is ${sub},
+MULTIPLICATION is ${mul},
+DIVISION is ${div}`);
+
+
+let firstName = "Nakul";
+let lastName = "Tanwar";
+lastName = 7;
+let fullName = firstName + lastName; // String Concatanation 
+console.log(fullName);
+
+
+
+// Arrays
+let arr = [1,2,3,4,5]
+console.log(arr)
+
+let add = arr[0] + arr[4];
+console.log(add);
+
+let arr1 = ['Nakul',true,1,[1,2,3], {name:'Nakul'}, undefined]
+console.log(arr1);
+
+console.log(arr1[4]['name']);
+
+
+let obj = {
+    a:1,
+    name:'Nakul',
+    status:false,
+    arr:[1,2,3,4]
+}
+console.log(obj);
+console.log(obj.arr[2]);
+
+
+let obj1 = {
+    a:1,
+    fname:"Nakul",
+    lname:"Tanwar"
+}
+console.log(obj1.fname + obj1.lname);
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Namaste-DSA
 In This Repo We are going to learn DSA From Namaste DSA by Akshay Saini
+- EP-01 Added 
+- Programming 101 (Basics of JS )
+- 1. write a program to print anything to console for that we use console.log("Namste DSA")
+
+-<strong>Data Types in JS</strong>
+- 1. Strings => ''."",``
+- 2. Numbers => 1,2...
+- 4. Boolean => true, false
+- 5. BigInit => 1n
+- 6. Symbol => ?
+- 7. undefined => undefined
+
+====================================
+- Types Declaration
+- 1. Const => can not re-declare or reassign
+- 2. let => can re-declare but can not reassign
+- 3. var => can be re-declare and  reassign
+
+
+====================================
+- 1. Arrays => Can store multiple value with diffrent data types
+- 2. Object => can store complex Data types in form of KAY and Value pair
+
